ab0698a0937a435e4df05943f0932cbfe15f0405
Raphael <raphaelndauwa@gmail.com>
2020-07-17 16:17:37 +0300

    validating inputs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,20 +9,46 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENTAGE = 100;
 
+        int principal = 0;
+        float yearlyRate = 0;
+        float rate = 0;
+        int numberOfPayments = 0;
+        byte periodYears = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter Principal ($1k - $1m): ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter amount between 1,000 and 1,000, 000");
+        }
 
-        System.out.print("Annual Interest rate: ");
-        float yearlyRate = scanner.nextFloat();
-        float rate = (yearlyRate/PERCENTAGE)/MONTHS_IN_YEAR;
+        while (true){
+            System.out.print("Annual Interest rate: ");
+            yearlyRate = scanner.nextFloat();
 
-        System.out.print("Enter Period (Years): ");
-        byte periodYears = scanner.nextByte();
-        int numberOfPayments = periodYears * MONTHS_IN_YEAR;
+            if (yearlyRate > 0 && yearlyRate <= 30) {
+                rate = (yearlyRate / PERCENTAGE) / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter value between 0 and 30.");
+        }
 
-        double mortgage = principal * ((rate *  Math.pow((1 + rate), numberOfPayments)) / (Math.pow((1+rate), numberOfPayments) - 1));
+        while (true){
+            System.out.print("Enter Period (Years): ");
+            periodYears = scanner.nextByte();
+
+            if (periodYears > 0 && periodYears <= 30){
+                numberOfPayments = periodYears * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter value between 0 and 30.");
+
+        }
+
+        double mortgage = principal * ((rate * Math.pow((1 + rate), numberOfPayments)) / (Math.pow((1 + rate), numberOfPayments) - 1));
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
 

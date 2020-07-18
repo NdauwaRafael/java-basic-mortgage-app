b7ace8b16cd4de186fc34581c71bb111d270f1bc
Raphael <raphaelndauwa@gmail.com>
2020-07-19 00:12:29 +0300

    refactore print methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,20 +13,29 @@ public class Main {
         float yearlyRate = (float) readNumber("Annual Interest rate: ", 0, 30);
         byte periodYears = (byte) readNumber("Enter Period (Years): ", 0, 30);
 
+        printMortgage(principal, yearlyRate, periodYears);
+
+        printPaymentSchedule(principal, yearlyRate, periodYears);
+
+    }
+
+    private static void printMortgage(int principal, float yearlyRate, byte periodYears) {
         double mortgage = calculateMortgage(principal, yearlyRate, periodYears);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("---------");
         System.out.println("Monthly Payment: " + mortgageFormatted);
+    }
 
+    private static void printPaymentSchedule(int principal, float yearlyRate, byte periodYears) {
         //B = L[(1 + c)n - (1 + c)p]/[(1 + c)n - 1]
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for (short month = 1; month <= periodYears * MONTHS_IN_YEAR; month++) {
             double balance = getBalance(principal, periodYears, yearlyRate, month);
-            String balanceFormatted = NumberFormat.getCurrencyInstance().format(balance)
+            String balanceFormatted = NumberFormat.getCurrencyInstance().format(balance);
             System.out.println(balanceFormatted);
         }
     }
@@ -34,6 +43,7 @@ public class Main {
     public static double readNumber(String prompt, double min, double max) {
         Scanner scanner = new Scanner(System.in);
         double value;
+
         while (true) {
             System.out.print(prompt);
             value = scanner.nextInt();
@@ -45,7 +55,6 @@ public class Main {
     }
 
     public static double calculateMortgage(int principal, float yearlyRate, byte periodYears) {
-
         int numberOfPayments = periodYears * MONTHS_IN_YEAR;
         float rate = (yearlyRate / PERCENTAGE) / MONTHS_IN_YEAR;
 
@@ -58,7 +67,6 @@ public class Main {
         float rate = (yearlyRate / PERCENTAGE) / MONTHS_IN_YEAR;
 
         double balance = (principal * (Math.pow((1 + rate), numberOfMonths) - Math.pow((1 + rate), numberOfPaymentsMade))) / (Math.pow((1 + rate), numberOfMonths) - 1);
-
         return balance;
 
     }

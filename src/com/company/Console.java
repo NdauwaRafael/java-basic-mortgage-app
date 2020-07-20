22a3ab271c556b8c0cda74ed270c995216382a02
Raphael <raphaelndauwa@gmail.com>
2020-07-20 18:38:34 +0300

    Overload readnumber method

--- src/com/company/Console.java
+++ src/com/company/Console.java
@@ -3,13 +3,19 @@ package com.company;
 import java.util.Scanner;
 
 public class Console {
+    public static double readNumber(String prompt) {
+        System.out.print(prompt);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, double min, double max) {
         Scanner scanner = new Scanner(System.in);
         double value;
 
         while (true) {
             System.out.print(prompt);
-            value = scanner.nextInt();
+            value = scanner.nextDouble();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter amount between " + min + " and " + max);
